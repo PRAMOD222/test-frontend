8d1e3c0690639094c52a7134a0f7bcde40200ce6
PRAMOD222 <pramodkesarkar2222@gmail.com>
2024-10-08 14:14:57 +0530

    config file change for image

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'ecom.thirstymart.in'],
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/products/all',  // Your frontend request path
+                destination: 'http://ecom.thirstymart.in/api/products/all',  // HTTP API being proxied
+            },
+        ];
+    },
 };
 
 export default nextConfig;
